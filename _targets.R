a359f47b78f13a06b2fc9339ab874f8e46683a37
Smith <jsmith@usgs.gov>
2021-08-24 16:53:31 -0400

    Edited filepaths

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library(targets)
-source('./1_fetch/src/getData.R')
-source('./2_process/src/processData.R')
-source('./3_visualize/src/plotDiagnostics.R')
+source('1_fetch/src/getData.R')
+source('2_process/src/processData.R')
+source('3_visualize/src/plotDiagnostics.R')
 tar_option_set(packages = c("tidyverse", "stringr", "sbtools", "whisker"))
 
 list(
@@ -9,7 +9,7 @@ list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    getData(outDir = "./1_fetch/out", outFileName = "model_RMSEs.csv"),
+    getData(outDir = "1_fetch/out", outFileName = "model_RMSEs.csv"),
     format = "file"
   ), 
   # Load the data
@@ -26,20 +26,20 @@ list(
   # Save the processed data
   tar_target(
     model_summary_results_csv,
-    writeSummaryData(data = eval_data, outDir = "./2_process/out", fileName = "model_summary_results.csv"), 
+    writeSummaryData(data = eval_data, outDir = "2_process/out", fileName = "model_summary_results.csv"), 
     format = "file"
   ),
   # Save the model diagnostics
   tar_target(
     model_diagnostic_text_txt,
-    writeSummaryText(data = eval_data, outDir = "./2_process/out", fileName = "model_diagnostic_text.txt"), 
+    writeSummaryText(data = eval_data, outDir = "2_process/out", fileName = "model_diagnostic_text.txt"), 
     format = "file"
   ),
   #3_visualize----
   # Create a plot
   tar_target(
     figure_1_png,
-    plotDiagnostics(data = eval_data, outDir = "./3_visualize/out", figName = "figure_1.png"), 
+    plotDiagnostics(data = eval_data, outDir = "3_visualize/out", figName = "figure_1.png"), 
     format = "file"
   )
 )
